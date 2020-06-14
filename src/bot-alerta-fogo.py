771ab1efb945078e14416dce861d2c31d52c73dd
Maiky Nata <maikynata25@gmail.com>
2020-06-14 13:13:33 -0300

    raise exceptions with requests.exceptions.Request

--- src/bot-alerta-fogo.py
+++ src/bot-alerta-fogo.py
@@ -3,7 +3,7 @@ import requests
 resp = requests.get('http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api/auxiliar/paises')
 if resp.status_code != 200:
     # This means something went wrong.
-    raise ApiError('GET /auxiliar/paises/ {}'.format(resp.status_code))
+    raise requests.exceptions.RequestException('GET /auxiliar/paises/ {}'.format(resp.status_code))
 for todo_item in resp.json():
     print('{} {}'.format(todo_item['pais_id'], todo_item['pais_name']))
 
