c65877b48bb4db05243e9bf7d300768767421953
Maiky Nata <maikynata25@gmail.com>
2020-06-14 12:52:31 -0300

    request and response ok GET /auxiliar/paises/

--- src/bot-alerta-fogo.py
+++ src/bot-alerta-fogo.py
@@ -1,8 +1,9 @@
 import requests
 
-resp = requests.get('https://todolist.example.com/tasks/')
+resp = requests.get('http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api/auxiliar/paises')
 if resp.status_code != 200:
     # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    raise ApiError('GET /auxiliar/paises/ {}'.format(resp.status_code))
 for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+    print('{} {}'.format(todo_item['pais_id'], todo_item['pais_name']))
+
