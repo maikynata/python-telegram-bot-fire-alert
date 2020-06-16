478c0f5c12f32ef0cc8eb9864bd5aeab9eb654b5
Thales Fossalussa <thalesvitor2001@gmail.com>
2020-06-16 10:01:24 -0300

    Added the focus's data to bot in Cavalcante-GO

--- src/bot-alerta-fogo.py
+++ src/bot-alerta-fogo.py
@@ -1,11 +1,24 @@
 import requests
 
-baseURL = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api'
+pais_id = int(33)
+estado_id = int(52)
+municipio_id = int(5205307)
 
-resp = requests.get(baseURL + '/auxiliar/paises')
-if resp.status_code != 200:
+coordenatesURL = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api/focos/?pais_id={}&estado_id={}&municipio_id={}'.format(pais_id, estado_id, municipio_id)
+countURL = 'http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api/focos/count?pais_id={}&estado_id={}&municipio_id={}'.format(pais_id, estado_id, municipio_id)
+
+respCount = requests.get(countURL)
+respCoordenates = requests.get(coordenatesURL)
+
+if respCoordenates.status_code != 200 or respCount.status_code != 200:
     # This means something went wrong.
-    raise requests.exceptions.RequestException('GET /auxiliar/paises/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['pais_id'], todo_item['pais_name']))
+    raise requests.exceptions.RequestException('GET /focos/ {}'.format(respCoordenates.status_code))
+    raise requests.exceptions.RequestException('GET /focos/count/ {}'.format(respCount.status_code))
+else:
+    print("Sucess")
 
+count = respCount.json()
+print('Numero de supostos focos: {}'.format(count['Brasil']))
+
+for todo_item in respCoordenates.json():
+    print('municipio = {}, localizacao = {}, {}'.format(todo_item['properties']['municipio'], todo_item['properties']['latitude'], todo_item['properties']['longitude']))
