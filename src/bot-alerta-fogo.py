235b4f4b136db670047f4e4b54daf65a6065ac76
Maiky Nata <maikynata25@gmail.com>
2020-07-14 15:29:02 -0300

    rename Coordinates word

--- src/bot-alerta-fogo.py
+++ src/bot-alerta-fogo.py
@@ -5,23 +5,23 @@ pais_id = int(33)
 estado_id = int(52)
 municipio_id = int(5205307)
 
-coordenatesURL = baseURL + '/focos/?pais_id={}&estado_id={}&municipio_id={}'.format(pais_id, estado_id, municipio_id)
+coordinatesURL = baseURL + '/focos/?pais_id={}&estado_id={}&municipio_id={}'.format(pais_id, estado_id, municipio_id)
 countURL = baseURL + '/focos/count?pais_id={}&estado_id={}&municipio_id={}'.format(pais_id, estado_id, municipio_id)
 
 respCount = requests.get(countURL)
-respCoordenates = requests.get(coordenatesURL)
+respCoordinates = requests.get(coordinatesURL)
 
-if respCoordenates.status_code != 200 or respCount.status_code != 200:
+if respCoordinates.status_code != 200 or respCount.status_code != 200:
     # This means something went wrong.
-    raise requests.exceptions.RequestException('GET /focos/ {}'.format(respCoordenates.status_code))
+    raise requests.exceptions.RequestException('GET /focos/ {}'.format(respCoordinates.status_code))
     raise requests.exceptions.RequestException('GET /focos/count/ {}'.format(respCount.status_code))
 else:
-    print("Sucess")
+    print("Success")
 
 count = respCount.json()
 print('Numero de supostos focos: {}'.format(count['Brasil']))
 
-for todo_item in respCoordenates.json():
+for todo_item in respCoordinates.json():
     print('municipio = {}, localizacao = {}, {}'.format(todo_item['properties']['municipio'],
                                                         todo_item['properties']['latitude'],
                                                         todo_item['properties']['longitude']))
