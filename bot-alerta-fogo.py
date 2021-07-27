650458fe2440bda1bb2b8167e1b77645ca369fcf
maikynata <maikynata25@gmail.com>
2021-07-27 20:36:45 -0300

    teste

--- bot-alerta-fogo.py
+++ bot-alerta-fogo.py
@@ -196,7 +196,6 @@ def estado(update, context):
     else:
         message = "Muito obrigado!"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE2
 
 
 
