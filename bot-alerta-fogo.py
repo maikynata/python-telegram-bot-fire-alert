7cddeffe59549092dfdce6ece650a8d3039138d7
maikynata <maikynata25@gmail.com>
2021-07-27 20:45:16 -0300

    estado reply test

--- bot-alerta-fogo.py
+++ bot-alerta-fogo.py
@@ -196,23 +196,11 @@ def estado(update, context):
     else:
         message = "Muito obrigado!"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE2
+        result_focos(update, context, cidade_resp)
 
 
 
-def result_focos(update, context):
-    cidade_resp = update.message.text
-
-    if len(cidade_resp) < 3:
-        message = """O nome da cidade está muito curto... 
-                        \nInforma mais pra gente, por favor?"""
-        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE1
-    else:
-        message = "Muito obrigado!"
-        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
+def result_focos(update, context, cidade_resp):
     message = 'Agora digite o nome por extenso, do estado deste município. Exemplo: Goiás.'
     update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
 
@@ -281,8 +269,7 @@ def main():
     conversation_handler = ConversationHandler(
         entry_points=[CommandHandler('iniciar', welcome)],
         states={
-            STATE1: [MessageHandler(Filters.text, estado)],
-            STATE2: [MessageHandler(Filters.text, result_focos)]
+            STATE1: [MessageHandler(Filters.text, estado)]
         },
         fallbacks=[CommandHandler('cancel', cancel)])
     updater.dispatcher.add_handler(conversation_handler)
