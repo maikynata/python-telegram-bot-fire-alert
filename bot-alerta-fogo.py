2b9938a08f6e5679090fbe5e1c4de9230c40d88c
maikynata <maikynata25@gmail.com>
2021-07-30 17:43:56 -0300

    state2 try result

--- bot-alerta-fogo.py
+++ bot-alerta-fogo.py
@@ -195,6 +195,7 @@ def estado(update, context):
         return STATE1
     else:
         message = "Muito obrigado!"
+        "Agora digite o nome por extenso, do estado deste município. Exemplo: Goiás"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         return STATE2
 
@@ -203,7 +204,7 @@ def result_focos(update, context):
 
     cidade_resp = update.message.text
     try:
-        message = 'Agora digite o nome por extenso, do estado deste município. Exemplo: Goiás.\n\n'
+        message = 'Obrigado!.\n\n'
         update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
     except Exception as e:
         print(str(e))
