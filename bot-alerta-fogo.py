e0dfd0d2341dc966b645b52d29f4bf968375c41e
maikynata <maikynata25@gmail.com>
2021-07-31 12:48:03 -0300

    ajuda end

--- bot-alerta-fogo.py
+++ bot-alerta-fogo.py
@@ -277,6 +277,7 @@ def getNota(update, context):
 def ajuda(update, context):
     message = 'Os dados apresentados pelo Labareda Alerta são atualizados a cada 3 horas, nos seguintes horários: 00:00, 03:00, 06:00, 09:00, 12:00, 15:00, 18:00, 21:00 (UTC) Conforme o site http://queimadas.dgi.inpe.br.'
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    return ConversationHandler.END
 
 
 def cancel(update, context):
