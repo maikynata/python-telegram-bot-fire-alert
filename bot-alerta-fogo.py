1ebb8e3e7f90d1226291b6f3e3a832c99472a6e6
maikynata <maikynata25@gmail.com>
2021-08-05 10:34:48 -0300

    fix message não ha focos

--- bot-alerta-fogo.py
+++ bot-alerta-fogo.py
@@ -322,6 +322,7 @@ def result_focos(update, context):
         else:
             message = 'Não há focos de incêndio registrados na região deste Município.'
             print(message)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
             endmessage = 'Consulta finalizada, utilize o menu para fazer uma nova consulta.'
             context.bot.send_message(chat_id=update.effective_chat.id, text=endmessage)
