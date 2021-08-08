c77e411952f9677cb45823733642b8f59ea3e5dd
maikynata <maikynata25@gmail.com>
2021-08-08 16:19:47 -0300

    serve static file flask

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route("/")
+@app.route("/index")
 def index():
     
     # Load current count
