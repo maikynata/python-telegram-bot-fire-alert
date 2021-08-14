a8954f477acb5d9e6ca84110bd61c676a42916b7
maikynata <maikynata25@gmail.com>
2021-08-14 12:19:53 -0300

    test

--- app.py
+++ app.py
@@ -5,19 +5,6 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 @app.route("/")
 def index():
     
-    # Load current count
-    f = open("count.txt", "r")
-    count = int(f.read())
-    f.close()
-
-    # Increment the count
-    count += 1
-
-    # Overwrite the count
-    f = open("count.txt", "w")
-    f.write(str(count))
-    f.close()
-
     # Render HTML with count variable
     return app.send_static_file("index.html")
 
